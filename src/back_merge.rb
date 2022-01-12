37d0437cfa3fde8ca2e8a98cc7a486989263bca7
Elijah Biede <6586509+hbiede@users.noreply.github.com>
2022-01-12 10:32:48 -0600

    Initial create

---
+++ src/back_merge.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+def commit_message
+  if ENV['COMMIT_MESSAGE'].empty?
+    "Merged #{ENV['SRC_BRANCH']} into #{ENV['DEST_BRANCH']}"
+  else
+    ENV['COMMIT_MESSAGE'].gsub('"', '\"')
+  end
+end
+
+def main
+  system('git config user.email github-actions@github.com && git config user.name github-actions')
+  system("git checkout #{ENV['DEST_BRANCH']}")
+  system("git merge #{ENV['SRC_BRANCH']} --strategy-option theirs -m \"#{commit_message}\"")
+  system("git push -u origin #{ENV['DEST_BRANCH']}")
+end
+
+main if __FILE__ == $PROGRAM_NAME
